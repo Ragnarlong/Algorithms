3d74677c6c18ade77687f2526564bc9f601525c3
Long Liu <long.liu@intel.com>
2019-10-10 16:11:29 +0800

    list draft version
    Signed-off-by: Long Liu <long.liu@intel.com>

---
+++ DataStru/list.h
@@ -0,0 +1,45 @@
+#ifndef LIST_H_ 
+#define LIST_H_
+
+
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+/*
+ * The C language struct can contain the pointer to 
+ * it's own struct
+ * */
+struct list_head {
+	struct list_head *prev, *next;
+};
+
+#define INIT_LIST_HEAD(ptr) do { (ptr)->next = (ptr); (ptr->prev) = (ptr); } \
+	while (0)
+
+
+static inline void list_add_node(struct list_head *new_node, struct list_head *prev, struct list_head *next)
+{
+	next->prev = new_node;
+	new_node->next = next;
+	prev->next = new_node;
+	new_node->prev = prev;
+}
+
+static inline void list_add(struct list_head *new_node, struct list_head *head)
+{
+	list_add_node(new_node, head, head->next);
+}
+
+
+static inline void list_add_tail(struct list_head *new_node, struct list_head *head)
+{
+	list_add_node(new_node, head->prev, head);
+}
+
+static inline void list_del_node(struct list_head *prev, struct list_head *next)
+{
+	next->prev = prev;
+	prev->next =next;
+}
+
+
